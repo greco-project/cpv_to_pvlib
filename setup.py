2c66df1c0f1f1ba9f294ded53711455aaa1a1eda
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2019-11-28 16:09:16 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='cpv_to_pvlib',
+    author='Inia Steinbach',
+    author_email='inia.steinbach@rl-institut.de',
+    description='A cpv module that could be added to pvlib.',
+    namespace_package=['cpv_to_pvlib'],
+    long_description=read('README.rst'),
+    packages=find_packages(),
+    package_dir={'cpv_to_pvlib': 'cpv_to_pvlib'},
+    extras_require={
+          'dev': ['sphinx', 'sphinx_rtd_theme', 'requests']},
+    install_requires=[
+        'pvlib >= 0.6.3',
+        'pandas >= 0.24.1',
+        'numpy',
+        'matplotlib'])
